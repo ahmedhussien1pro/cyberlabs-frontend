2e5de0afef8fbbb072353800bba330a523ffcc96
Eng-Ahmed-Hussien <Ahmed.hussien685@el-eng.menofia.edu.eg>
2026-02-15 17:47:56 +0200

    feat(auth): refactor pages 4

--- src/features/auth/hooks/index.ts
+++ src/features/auth/hooks/index.ts
@@ -1,5 +1,3 @@
 // Auth custom hooks
 export { useResendTimer } from './useResendTimer';
 export { usePasswordStrength } from './usePasswordStrength';
-export * from './useAuthRedirect';
-export * from './useOTPInput';
